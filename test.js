fed22fff13e318588d7fe8e19dd97d99b628f466
Jerome <naronkrach@gmail.com>
2022-08-10 20:22:27 +0700

    Learned to fetch data from api

--- test.js
+++ test.js
@@ -8,3 +8,12 @@ function displayMessage(msg) {
 let greetingMsg = "Welcome to the world of Javascript";
 
 displayMessage(greetingMsg);
+async function getData() {
+  const data = await fetch(
+    "https://api.dictionaryapi.dev/api/v2/entries/en/family"
+  );
+  const json = await data.json();
+  console.log(json[0].meanings[0].definitions[0].definition);
+}
+
+getData();
