174346869b600b2144c98c6d5f63a0573ce0168b
Jerome <naronkrach@gmail.com>
2022-08-10 15:37:09 +0700

    First day task

---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+function displayMessage(msg) {
+  console.log("starting...function");
+  console.log(msg);
+  console.log("ending...function");
+}
+
+let greetingMsg = "Welcome to the world of Javascript";
+
+displayMessage(greetingMsg);
