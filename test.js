c3bc9381ae28caf04018fecd40bfb730f6aa2d5c
Jerome <naronkrach@gmail.com>
2022-08-17 13:45:47 +0700

    Tried to fetch api

--- test.js
+++ test.js
@@ -1,19 +1,28 @@
-console.log("Hello World");
-function displayMessage(msg) {
-  console.log("starting...function");
-  console.log(msg);
-  console.log("ending...function");
+// console.log("Hello World");
+// function displayMessage(msg) {
+//   console.log("starting...function");
+//   console.log(msg);
+//   console.log("ending...function");
+// }
+
+// let greetingMsg = "Welcome to the world of Javascript";
+
+// displayMessage(greetingMsg);
+const searchInput = document.querySelector("input");
+
+searchInput.addEventListener("input", getData);
+
+async function getData(e) {
+  const word = e.target.value;
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((res) => {
+      if (!res.ok) {
+        console.log("Response Error");
+        return;
+      }
+      return res.json();
+    })
+    .then((data) => {
+      data && console.log(data[0].meanings[0].definitions[0].definition);
+    });
 }
-
-let greetingMsg = "Welcome to the world of Javascript";
-
-displayMessage(greetingMsg);
-async function getData() {
-  const data = await fetch(
-    "https://api.dictionaryapi.dev/api/v2/entries/en/family"
-  );
-  const json = await data.json();
-  console.log(json[0].meanings[0].definitions[0].definition);
-}
-
-getData();
